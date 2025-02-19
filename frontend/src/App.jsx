1e79b85c3a018700186a8a939f6b10ef8cfd78ed
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2025-02-19 10:13:18 +0400

    Include a Next Joke button with smooth navigation between jokes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,12 @@ import Joke from "./components/Joke"
 import './App.css'
 
 function App() {
-  const [loading, setLoading] = useState(true);
-      
+  const [loading, setLoading] = useState(true)
+  const nextJoke = () => if (loading) setLoading(false)
   return <>
       <h1>Jokes Voting</h1>
       <Joke loading={loading} />
+      <button onClick={nextJoke} disabled={loading} id="next">Next Joke</button>
       <footer>
         <p className="read-the-docs">DataArt Winter IT Camp 2025 JavaScript Workshop</p>
         <p className="read-the-docs">Modern JS App Development: React, Node.js</p>
