7bad91753eda01de3eac12b0cef97afb700b2ada
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2025-02-19 08:11:06 +0400

    create Express API endpoints and CORS configuration

---
+++ backend/jokes.js
@@ -0,0 +1,25 @@
+export default [{
+	"id": "holy-moly",
+	"question": "Why did the baker stop selling doughnuts?",
+	"answer": "Got tired of the hole thing.",
+},
+{
+	"id": "no-sql",
+	"question": "A couple of relational database admins walked into a NoSQL bar. A little while later they walked out.",
+	"answer": "Because they couldn't find a table!",
+},
+{
+	"id": "kind-star",
+	"question": "What kind of star could hurt you?",
+	"answer": "A shooting star.",
+},
+{
+	"id": "spider-shop",
+	"question": "Where do spiders shop?",
+	"answer": "The Web.",
+},
+{
+	"id": "sink",
+	"question": "What do you call 10 000 lawyers at the bottom of the sea?",
+	"answer": "A boating accident.",
+}]
